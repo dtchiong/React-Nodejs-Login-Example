a60b8f5c142c8a3bd23e6d79459d042dddbc7965
Derek Chiong <dtchiong@gmail.com>
2019-02-06 19:28:53 -0800

    Added onChange function for form input. Centered login control with css

--- client/src/components/Login/LoginController.js
+++ client/src/components/Login/LoginController.js
@@ -3,17 +3,29 @@ import { Button, Form } from "react-bootstrap";
 import "./LoginController.css";
 class LoginController extends Component {
   state = {
-    email: "",
+    username: "",
     password: ""
   };
 
+  /* Sets the state when the form's input changes */
   handleChange = e => {
-    console.log("changed");
+    const id = e.target.id;
+    const val = e.target.value;
+    switch(id) {
+      case "formGroupUsername":
+        this.setState( {username: val} );
+        break;
+      case "formGroupPassword":
+        this.setState( {password: val } );
+        break;
+      default:
+        break;
+    }
   };
 
   render() {
     return (
-      <div className="form-login-container d-flex align-items-center">
+      <div className="form-login-wrapper d-flex align-items-center d-flex justify-content-center">
         <Form className="form-login " >
           <h1>Login</h1>
           <Form.Group controlId="formGroupUsername">
@@ -34,6 +46,7 @@ class LoginController extends Component {
             >
             </Form.Control>
           </Form.Group>
+          <div id="login-button-border"></div>
           <Button variant="primary" block>Log In</Button>
         </Form>
       </div>
